1f090a3324c1cada2c2df4d562e1ee197c92bfb0
Alok-Sharma <alok.sharma127@gmail.com>
2015-11-30 15:17:57 -0500

    skeleton for user actor

--- src/main/scala/ufl/Client.scala
+++ src/main/scala/ufl/Client.scala
@@ -14,37 +14,64 @@ import spray.can.Http
 case object postUser
 case object postPost
 case object getUser
+case object execute
+case class startClient(numOfUsers:Int, heavyUsers:Int)
 
-// class Client extends Actor {
-  
-// }
+class Client extends Actor {
+  def receive = {
+    case startClient(numOfUsers, heavy) => {
+      var heavyUsers = heavy
+      val actorSystem = ActorSystem("user-system")
+      //todo percentages
+      for(userNumber <- 0 to numOfUsers) {
+        heavyUsers = heavyUsers - 1
+        if(heavyUsers > 0) {
+          val user = actorSystem.actorOf(Props(new UserActor(true)), userNumber)  
+          user ! execute
+        } else {
+          val user = actorSystem.actorOf(Props(new UserActor(false)), userNumber)
+          user ! execute
+        }
+        
+      }
+    }
+  }  
+}
 
-class UserActor extends Actor {
+class UserActor(isHeavy:Boolean) extends Actor {
   val system = ActorSystem("ClientSystem")
   import system.dispatcher
   import ufl.FacebookAPI._
   var id:String = _
   val pipeline2 = sendReceive ~> unmarshal[String]
   val pipeline1 = sendReceive ~> unmarshal[UserResponse]
+
+  createUser
+
   def receive = {
 
-    case `postUser` => {
-      var user: User = new User("nikita@babies.com", "chotu", "baby", "B")
-      println("sent post")
-      val responseFuture = pipeline2(Post("http://localhost:5000/user", user))
-      responseFuture onComplete {
-        case Success(result) =>
-          println(result)
-      }
-    }
-    case `getUser` => {
-      println("get user")
-      val responseFuture1 = pipeline1(Get("http://localhost:5000/user/1"))
+    case `execute` => {
 
-      responseFuture1 onComplete {
-        case Success(UserResponse(id, email, first_name, last_name, gender, posts, albums, friends)) =>
-          println(email)
-      }
+    }
+  }
+
+  def createUser = {
+    var user: User = new User("nikita@babies.com", "chotu", "baby", "B")
+    println("sent post")
+    val responseFuture = pipeline2(Post("http://localhost:5000/user", user))
+    responseFuture onComplete {
+      case Success(result) =>
+        println(result)
+    }
+  }
+
+  def getUser = {
+    println("get user")
+    val responseFuture1 = pipeline1(Get("http://localhost:5000/user/1"))
+
+    responseFuture1 onComplete {
+      case Success(UserResponse(id, email, first_name, last_name, gender, posts, albums, friends)) =>
+        println(email)
     }
   }
 }
